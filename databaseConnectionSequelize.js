590f3881eb1d173e1849aea9c586f1c34f1b28e4
Evann Li <evannli450@gmail.com>
2024-03-21 13:58:20 -0700

    update db and routes

---
+++ databaseConnectionSequelize.js
@@ -0,0 +1,13 @@
+const is_render = process.env.IS_RENDER || false; 
+
+const dbConfigRender = "mysql://freedb_2350_main:SA33#V&vwb&!GUm@sql.freedb.tech:3306/ freedb_comp2350-week2-A01274308";
+const dbConfigLocal = "mysql://root:El11fordawin@localhost/lab_example";
+
+if (is_render) {
+    var databaseConnectionString = dbConfigRender;
+}
+else {
+    var databaseConnectionString = dbConfigLocal;
+}
+
+module.exports = databaseConnectionString;
