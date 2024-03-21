7ccb991e580f3c57ace91be152cf69864b13d115
Evann Li <evannli450@gmail.com>
2024-03-21 14:10:46 -0700

    updated link

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,13 +1,32 @@
 const is_render = process.env.IS_RENDER || false; 
 
-const dbConfigRender = "mysql://freedb_2350_main:SA33#V&vwb&!GUm@sql.freedb.tech:3306/freedb_comp2350-week2-A01274308";
-const dbConfigLocal = "mysql://root:El11fordawin@localhost/lab_example";
+const dbConfigRender = {
+    dialect: 'mysql',
+    host: 'sql.freedb.tech',
+    port: 3306,
+    username: 'freedb_2350_main',
+    password: 'SA33#V&vwb&!GUm',
+    database: 'freedb_comp2350-week2-A01274308',
+};
 
+const dbConfigLocal = {
+    dialect: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'El11fordawin',
+    database: 'lab_example',
+};
+
+let databaseConnectionString;
 if (is_render) {
-    var databaseConnectionString = dbConfigRender;
-}
-else {
-    var databaseConnectionString = dbConfigLocal;
+    databaseConnectionString = process.env.RENDER_DB_CONNECTION_STRING; // Assuming Render provides a DB connection string as an environment variable
+} else {
+    databaseConnectionString = 'mysql://root:El11fordawin@localhost/lab_example';
 }
 
-module.exports = databaseConnectionString;
+module.exports = {
+    dbConfigRender,
+    dbConfigLocal,
+    databaseConnectionString,
+};
