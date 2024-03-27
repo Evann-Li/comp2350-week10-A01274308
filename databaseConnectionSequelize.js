a1e1d6ef317f0fa0fd09d83d874ed0e0c6bf7898
Evann Li <evannli450@gmail.com>
2024-03-27 13:11:57 -0700

    update

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,32 +1,13 @@
 const is_render = process.env.IS_RENDER || false; 
 
-const dbConfigRender = {
-    dialect: 'mysql',
-    host: 'sql.freedb.tech',
-    port: 3306,
-    username: 'freedb_2350_main',
-    password: 'SA33#V&vwb&!GUm',
-    database: 'freedb_comp2350-week2-A01274308',
-};
+const dbConfigRender = "mysql://freedb_2350_main:SA33#V&vwb&!GUm@sql.freedb.tech/freedb_comp2350-week2-A01274308";
+const dbConfigLocal = "mysql://root:El11fordawin@localhost/lab_example";
 
-const dbConfigLocal = {
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'El11fordawin',
-    database: 'lab_example',
-};
-
-let databaseConnectionString;
 if (is_render) {
-    databaseConnectionString = process.env.RENDER_DB_CONNECTION_STRING; // Assuming Render provides a DB connection string as an environment variable
-} else {
-    databaseConnectionString = 'mysql://root:El11fordawin@localhost/lab_example';
+    var databaseConnectionString = dbConfigRender;
+}
+else {
+    var databaseConnectionString = dbConfigLocal;
 }
 
-module.exports = {
-    dbConfigRender,
-    dbConfigLocal,
-    databaseConnectionString,
-};
+module.exports = databaseConnectionString;
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const userModel = include('models/web_user');
+const crypto = require('crypto');
+const {v4: uuid} = require('uuid');
+const passwordPepper = "SeCretPeppa4MySal+"; 
 // const database = include('databaseConnection');
 // const dbModel = include('databaseAccessLayer');
 //const dbModel = include('staticData');
@@ -37,6 +40,31 @@ router.get('/', async (req, res) => {
 		res.render('error', {message: 'Error connecting to MySQL'}); 
 		console.log("Error connecting to MySQL"); 
 		console.log(ex); 
+	} 
+}); 
+
+router.post('/addUser', async (req, res) => {  
+	try {   
+		console.log("form submit");    
+		const password_salt = crypto.createHash('sha512');    
+		password_salt.update(uuid());      
+		const password_hash = crypto.createHash('sha512');    
+		password_hash.update(req.body.password+passwordPepper+password_salt);     
+		let newUser = userModel.build(    
+			{      
+				first_name: req.body.first_name,     
+				last_name: req.body.last_name,     
+				email: req.body.email,     
+				password_salt: password_salt.digest('hex'),     
+				password_hash: password_hash.digest('hex')    
+			}   
+		);   
+		await newUser.save();   
+		res.redirect("/");  
+	}  catch(ex) {   
+		res.render('error', {message: 'Error connecting to MySQL'});   
+		console.log("Error connecting to MySQL");   
+		console.log(ex);   
 	} 
 }); 
 
